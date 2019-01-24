2919bdca9048a3f55bd9be3a4a7c4d2ac643d936
Jetro Hulkko <jetro.hulkko@helsinki.fi>
2019-01-25 00:14:41 +0200

    valmistelua tietokantaa varten

--- application/__init__.py
+++ application/__init__.py
@@ -9,4 +9,6 @@ db = SQLAlchemy(app)
 
 from application import views
 
+from application.submissions import models
+
 db.create_all()
---
+++ application/submissions/models.py
@@ -0,0 +1,14 @@
+from application import db
+
+class Submission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+    onupdate=db.func.current_timestamp())
+
+    name = db.Column(db.String(144), nullable=False)
+    code = db.Column(db.String(1440), nullable=False)
+
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
